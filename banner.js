01fc6a2d2721f57a148d8f403d05be5a0b4cdd04
Cheese <stuffalltech@live.co.uk>
2016-10-26 03:44:43 +0100

    Added "It's Time To SoftSoap" ad.

--- banner.js
+++ banner.js
@@ -3,7 +3,8 @@ $(document).ready(function(){
 var bannerArray = [
   {"url":"https://www.youtube.com/watch?v=POnldqfm3Rw","banner":"img/trailerad.png"},
   {"url":"https://www.youtube.com/watch?v=POnldqfm3Rw","banner":"img/malloryad.png"},
-  {"url":"https://clubwump.us/","banner":"img/electionad.png"}]
+  {"url":"https://clubwump.us/","banner":"img/electionad.png"},
+  {"url":"https://soundcloud.com/cheesecast/its-time-to-softsoap","banner":"img/songad.png"}]
 
 var selectBanner = bannerArray[Math.floor(Math.random()*bannerArray.length)];
 
