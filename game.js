5da5e0c2eb8e9a00ffbde19ef95772ebe1fa04e6
voctory <voctorbidoof@gmail.com>
2016-10-26 15:43:58 -0700

    Update game.js

--- game.js
+++ game.js
@@ -80,10 +80,10 @@ game.onload = function(){
 	game.rootScene.addChild(game.getready);
 	
 
-	game.flapButton = new Sprite(200,200);
+	game.flapButton = new Sprite(100,100);
 	game.flapButton.image = game.assets['assets/flap.png'];
 	game.flapButton.x = 100;
-	game.flapButton.y = 100;
+	game.flapButton.y = 1200;
 	
 	// adds flapButton to the rootScene
 	
