8b776bbb355e7274ffbcc6a68f9eca626c838510
Stoupedog <h.stoupe@gmail.com>
2016-10-26 16:47:34 +1300

    Tidying + commenting main.js

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 $(document).ready(function(){
 
-var bannerArray = [{"url":"https://www.youtube.com/watch?v=POnldqfm3Rw","banner":"img/trailerad.png"},{"url":"https://www.youtube.com/watch?v=POnldqfm3Rw","banner":"img/malloryad.png"},{"url":"https://clubwump.us/","banner":"img/electionad.png"},{"url":"https://soundcloud.com/cheesecast/its-time-to-softsoap","banner":"img/songad.png"},{"url":"https://www.youtube.com/watch?v=xUVz4nRmxn4","banner":"img/adplacement.png"}]
+// Displaying random banner ads below the game
+var bannerArray = [
+	{"url":"https://www.youtube.com/watch?v=POnldqfm3Rw","banner":"img/trailerad.png"},
+	{"url":"https://www.youtube.com/watch?v=POnldqfm3Rw","banner":"img/malloryad.png"},
+	{"url":"https://clubwump.us/","banner":"img/electionad.png"},
+	{"url":"https://soundcloud.com/cheesecast/its-time-to-softsoap","banner":"img/songad.png"},
+	{"url":"https://www.youtube.com/watch?v=xUVz4nRmxn4","banner":"img/adplacement.png"}]
 
 var selectBanner = bannerArray[Math.floor(Math.random()*bannerArray.length)];
 
 $('#bottom-banner__inner').html('<a target="_blank" href="' + selectBanner.url + '"><img src="' + selectBanner.banner + '" alt="Random Banner" /></a>');
 
-    var backgroundAudio=document.getElementById("bg-audio");
-    backgroundAudio.volume=0.5;
+// Reducing Volume
+var backgroundAudio=document.getElementById("bg-audio");
+backgroundAudio.volume=0.5;
   
 });
