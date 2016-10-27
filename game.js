86e6015511b38b4d7bfec9e229e1c49d7512f258
Cheese <stuffalltech@live.co.uk>
2016-10-27 21:56:01 +0100

    Fixes

--- game.js
+++ game.js
@@ -21,7 +21,7 @@ game.gravity = 0.5;
 game.flap_strength = 9;
 game.fly_speed = 3.5;
 game.obstacle_frequency = 50;
-game.keybind(32, 'jump');  
+game.keybind(32, 'jump');
 
 var obstacles = new Group();
 
@@ -39,24 +39,24 @@ var playingTrumpus = false;
 var gameEnded = false;
 
 // preload assets
-game.preload('assets/halloween/background.png',
-	'assets/blurple-vigenette.png',
-	'assets/gameover.png',
-	'assets/halloween/getready.png',
-	'assets/ground.png',
-	'assets/instructions.png',
-	'assets/mallorySoftSoap.png',
-	'assets/obstacle_top.png',
-	'assets/obstacle_bottom.png',
-	'assets/wumpusGolden.png',
-	'assets/wumpusRainbow.png',
-	'assets/wumpusTrump.png',
-	'assets/wumpusVanilla.png',
-	'assets/back2menu.png',
-	'assets/play.png',
-	'assets/flappywumpuslogo2.png',
-	'assets/retry.png');
-	
+game.preload('../assets/halloween/background.png',
+	'../assets/blurple-vigenette.png',
+	'../assets/gameover.png',
+	'../assets/halloween/getready.png',
+	'../assets/ground.png',
+	'../assets/instructions.png',
+	'../assets/mallorySoftSoap.png',
+	'../assets/obstacle_top.png',
+	'../assets/obstacle_bottom.png',
+	'../assets/wumpusGolden.png',
+	'../assets/wumpusRainbow.png',
+	'../assets/wumpusTrump.png',
+	'../assets/wumpusVanilla.png',
+	'../assets/back2menu.png',
+	'../assets/play.png',
+	'../assets/flappywumpuslogo2.png',
+	'../assets/retry.png');
+
 
 // initialize game
 function gameinit() {
@@ -70,76 +70,76 @@ function gameinit() {
 	// adds background back
 	game.rootScene.removeChild(game.bg);
 	game.rootScene.addChild(game.bg);
-	
+
   // add obstacles to rootScene
 	game.rootScene.addChild(obstacles);
 
   // add scoreBoard to rootScene
 	game.rootScene.addChild(scoreBoard);
-	
+
 	game.ground = new Sprite(1280,86);
-  	game.ground.image = game.assets['assets/ground.png'];
+  	game.ground.image = game.assets['../assets/ground.png'];
   	game.ground.x = 0;
   	game.ground.y = game.height - 48;
-  
+
   // add game.ground to rootScene
 	game.rootScene.addChild(game.ground);
 
   // add the start game screen
   game.getready = new Sprite(592,177);
-  game.getready.image = game.assets['assets/halloween/getready.png'];
+  game.getready.image = game.assets['../assets/halloween/getready.png'];
   game.getready.x = (game.width / 2) - (game.getready.width / 2);
   game.getready.y = (game.height / 2) - (game.getready.height / 2);
 	game.getready.buttonMode = "up"
 
   // add game.getready to rootScene
 
-	
-	game.rootScene.addChild(game.getready);	
-  
+
+	game.rootScene.addChild(game.getready);
+
   // add the main character
   // Adds a 10% chance to get special characters :p
   var randomNumber = Math.random() * 100 + 1;
   if (randomNumber <= 10) {
     game.avatar = new Sprite(87,55);
-    game.avatar.image = game.assets['assets/wumpusGolden.png'];
+    game.avatar.image = game.assets['../assets/wumpusGolden.png'];
   } else if (randomNumber <= 20) {
     game.avatar = new Sprite(87,55);
-    game.avatar.image = game.assets['assets/wumpusRainbow.png'];
+    game.avatar.image = game.assets['../assets/wumpusRainbow.png'];
   } else if (randomNumber <= 30) {
     playingTrumpus = true;
     game.avatar = new Sprite(87,55);
-    game.avatar.image = game.assets['assets/wumpusTrump.png'];
+    game.avatar.image = game.assets['../assets/wumpusTrump.png'];
   } else if (randomNumber <= 40) {
     game.avatar = new Sprite(87,85);
-    game.avatar.image = game.assets['assets/mallorySoftSoap.png'];
+    game.avatar.image = game.assets['../assets/mallorySoftSoap.png'];
   } else {
     game.avatar = new Sprite(87,55);
-    game.avatar.image = game.assets['assets/wumpusVanilla.png'];
+    game.avatar.image = game.assets['../assets/wumpusVanilla.png'];
   }
 
   game.avatar.x = 100;
   game.avatar.y = 295;
   game.avatar.ySpeed = 0;
-  
+
 
   // add game.avatar to rootScene
 	game.rootScene.addChild(game.avatar);
 
   // add the instructions
   game.instructions = new Sprite(420,22);
-  game.instructions.image = game.assets['assets/instructions.png'];
+  game.instructions.image = game.assets['../assets/instructions.png'];
   game.instructions.x = (game.width/2) - (game.instructions.width/2);
   game.instructions.y = 460;
 	game.instructions.buttonMode = "up";
-	
+
 	game.bg.buttonMode = "up";
 
   // add game.instructions to rootScene
 	game.rootScene.addChild(game.instructions);
 
   game.rootScene.addEventListener(enchant.Event.UP_BUTTON_DOWN, game_touched);
-	
+
 }
 
 function clearobstacles() {
@@ -152,9 +152,9 @@ function clearobstacles() {
 }
 
 function gamerestart() {
-	
+
 	// clearing sprites up
-	
+
 	game.rootScene.removeChild(game.bg);
 	game.rootScene.removeChild(game.playbutton);
 	game.rootScene.removeChild(game.flappylogo);
@@ -163,8 +163,8 @@ function gamerestart() {
 	game.rootScene.removeChild(game.menubutton);
 	game.rootScene.clearEventListener(enchant.Event.DOWN_BUTTON_DOWN);
 	game.rootScene.clearEventListener(enchant.Event.RIGHT_BUTTON_DOWN);
-	
-	
+
+
 	game.gravity = 0.5;
 	game.flap_strength = 9;
 	game.fly_speed = 3.5;
@@ -177,70 +177,70 @@ function gamerestart() {
 	scoreBoard.text = "0";
 	game.rootScene.removeChild(obstacles);
 	game.rootScene.clearEventListener(enchant.Event.UP_BUTTON_DOWN);
-	
+
 	// does stuff with the background
-	
+
 	game.bg.buttonMode = "up";
 	game.rootScene.addChild(game.bg)
-	
+
 	game.rootScene.addChild(obstacles);
 	game.ground = new Sprite(1280,86);
-  	game.ground.image = game.assets['assets/ground.png'];
+  	game.ground.image = game.assets['../assets/ground.png'];
   	game.ground.x = 0;
   	game.ground.y = game.height - 48;
-  
+
   	// add game.ground to rootScene
 	game.rootScene.addChild(game.ground);
 	// add the start game screen
   game.getready = new Sprite(592,177);
-  game.getready.image = game.assets['assets/halloween/getready.png'];
+  game.getready.image = game.assets['../assets/halloween/getready.png'];
   game.getready.x = (game.width / 2) - (game.getready.width / 2);
   game.getready.y = (game.height / 2) - (game.getready.height / 2);
 	game.getready.buttonMode = "up"
 
   // add game.getready to rootScene
 
-	
+
 	game.rootScene.addChild(game.getready);
-	
+
 	var randomNumber = Math.random() * 100 + 1;
   if (randomNumber <= 10) {
     game.avatar = new Sprite(87,55);
-    game.avatar.image = game.assets['assets/wumpusGolden.png'];
+    game.avatar.image = game.assets['../assets/wumpusGolden.png'];
   } else if (randomNumber <= 20) {
     game.avatar = new Sprite(87,55);
-    game.avatar.image = game.assets['assets/wumpusRainbow.png'];
+    game.avatar.image = game.assets['../assets/wumpusRainbow.png'];
   } else if (randomNumber <= 30) {
     playingTrumpus = true;
     game.avatar = new Sprite(87,55);
-    game.avatar.image = game.assets['assets/wumpusTrump.png'];
+    game.avatar.image = game.assets['../assets/wumpusTrump.png'];
   } else if (randomNumber <= 40) {
     game.avatar = new Sprite(87,85);
-    game.avatar.image = game.assets['assets/mallorySoftSoap.png'];
+    game.avatar.image = game.assets['../assets/mallorySoftSoap.png'];
   } else {
     game.avatar = new Sprite(87,55);
-    game.avatar.image = game.assets['assets/wumpusVanilla.png'];
+    game.avatar.image = game.assets['../assets/wumpusVanilla.png'];
   }
 
   game.avatar.x = 100;
   game.avatar.y = 295;
   game.avatar.ySpeed = 0;
-  
+
 
   // add game.avatar to rootScene
 	game.rootScene.addChild(game.avatar);
-	
+
 	game.instructions = new Sprite(420,22);
-  game.instructions.image = game.assets['assets/instructions.png'];
+  game.instructions.image = game.assets['../assets/instructions.png'];
   game.instructions.x = (game.width/2) - (game.instructions.width/2);
   game.instructions.y = 460;
 	game.instructions.buttonMode = "up"
 	game.rootScene.removeChild(scoreBoard)
 	game.rootScene.addChild(scoreBoard)
-	
+
 	// add game.instructions to rootScene
 	game.rootScene.addChild(game.instructions);
-	
+
 	game.rootScene.addEventListener(enchant.Event.UP_BUTTON_DOWN, game_touched);
 }
 
@@ -258,22 +258,22 @@ function openmenu() {
 	gameEnded = false;
 	clearobstacles()
 	game.playbutton = new Sprite(300,100);
-	game.playbutton.image = game.assets['assets/play.png'];
+	game.playbutton.image = game.assets['../assets/play.png'];
 	game.playbutton.y = game.height/2 + 50;
 	game.playbutton.x = game.width/2 - 150;
 	game.playbutton.buttonMode = "up"
-	
+
 	game.rootScene.addChild(game.playbutton);
-	
+
 	// adding the logo
-	
+
 	game.flappylogo = new Sprite(464,206);
-	game.flappylogo.image = game.assets['assets/flappywumpuslogo2.png'];
+	game.flappylogo.image = game.assets['../assets/flappywumpuslogo2.png'];
 	game.flappylogo.y = game.height/2 - 200;
 	game.flappylogo.x = game.width/2 - 225;
-	
+
 	game.rootScene.addChild(game.flappylogo);
-	
+
 	game.rootScene.addEventListener(enchant.Event.UP_BUTTON_DOWN, gameinit);
 	game.flappylogo.addEventListener(Event.ENTER_FRAME, function () {
 		if (logoVariable == false) {
@@ -304,40 +304,40 @@ function logoTimer() {
 
 game.onload = function(){
 	game.bg = new Sprite(1280,720);
-  game.bg.image = game.assets['assets/halloween/background.png'];
-  
+  game.bg.image = game.assets['../assets/halloween/background.png'];
+
   // add game.bg to rootScene
 	game.rootScene.addChild(game.bg);
-	
+
 	// add the floor
   game.ground = new Sprite(1280,86);
-  game.ground.image = game.assets['assets/ground.png'];
+  game.ground.image = game.assets['../assets/ground.png'];
   game.ground.x = 0;
   game.ground.y = game.height - 48;
-  
+
   // add game.ground to rootScene
 	game.rootScene.addChild(game.ground);
-	
+
 	// adding playbutton stuff
-	
+
 	game.playbutton = new Sprite(300,100);
-	game.playbutton.image = game.assets['assets/play.png'];
+	game.playbutton.image = game.assets['../assets/play.png'];
 	game.playbutton.y = game.height/2 + 50;
 	game.playbutton.x = game.width/2 - 150;
 	game.playbutton.buttonMode = "up"
-	
+
 	game.rootScene.addChild(game.playbutton);
-	
+
 	// adding the logo
-	
+
 	game.flappylogo = new Sprite(464,206);
-	game.flappylogo.image = game.assets['assets/flappywumpuslogo2.png'];
+	game.flappylogo.image = game.assets['../assets/flappywumpuslogo2.png'];
 	game.flappylogo.y = game.height/2 - 200;
 	game.flappylogo.x = game.width/2 - 225;
-	
+
 	game.rootScene.addChild(game.flappylogo);
 	logoTimer()
-	
+
 	game.rootScene.addEventListener(enchant.Event.UP_BUTTON_DOWN, gameinit);
 	game.flappylogo.addEventListener(Event.ENTER_FRAME, function () {
 		if (logoVariable == false) {
@@ -347,7 +347,7 @@ game.onload = function(){
 			game.flappylogo.y++
 		}
   });
-  
+
 } // end game.onload #initialize game
 
 // listen for tap/click/up arrow
@@ -355,14 +355,14 @@ function game_touched() {
   if(!game.started) {
       //start the game
     game.started = true;
-  
+
     // remove getready and instructions
-	
+
 }
-		
+
       game.rootScene.removeChild(game.getready);
     game.rootScene.removeChild(game.instructions);
-	
+
 	game.avatar.ySpeed = -game.flap_strength;
 //	flapAnimation()
       if(gameEnded == false) {
@@ -399,14 +399,14 @@ game.onenterframe = function(){
 
     // track flying progress
     game.distance += game.fly_speed;
-    
+
     // check if we need to spawn obstacle
     if(game.distance % game.obstacle_frequency == 0){
-      
+
       // spawn obstacle
       spawnObstacle()
     game.started = true;
-      
+
       // clean up old obstacles
       for (var i = 0; i < obstacles.childNodes.length; i++) {
         if(obstacles.childNodes[i].x + obstacles.x + 100 < -85){
@@ -435,7 +435,7 @@ game.onenterframe = function(){
         scoreBoard.text++;
       }
     }
-    
+
   }
 } // end game.onenterframe #game loop
 
@@ -445,7 +445,7 @@ function spawnObstacle(){
   var gap = 200;
 
   var top = new Sprite(85, 545);
-  top.image = game.assets['assets/obstacle_top.png'];
+  top.image = game.assets['../assets/obstacle_top.png'];
   top.x = -obstacles.x + game.width;
   top.y = -550 + pos - gap;
 	top.buttonMode = "up"
@@ -453,15 +453,15 @@ function spawnObstacle(){
   obstacles.addChild(top);
 
   var bottom = new Sprite(85, 545);
-  bottom.image = game.assets['assets/obstacle_bottom.png'];
+  bottom.image = game.assets['../assets/obstacle_bottom.png'];
   bottom.x = -obstacles.x + game.width;
   bottom.y = pos;
 	bottom.buttonMode = "up";
-  
+
   // add bottom obstacle to obstacles group
   obstacles.addChild(bottom);
 	//game.rootScene.addEventListener(enchant.Event.UP_BUTTON_DOWN, game_touched);
-  
+
 } // end spawnObstacle
 
 function gameover(){
@@ -471,26 +471,26 @@ function gameover(){
 	game.started = false;
 	game.rootScene.removeChild(game.flapButton);
   game.gameover = new Sprite(602,163);
-  game.gameover.image = game.assets['assets/gameover.png'];
+  game.gameover.image = game.assets['../assets/gameover.png'];
   game.gameover.x = (game.width/2) - (game.gameover.width/2);
   game.gameover.y = 120;
-	
+
 	// adding a retry button
 	game.retrybutton = new Sprite(300,100);
-	game.retrybutton.image = game.assets['assets/retry.png'];
+	game.retrybutton.image = game.assets['../assets/retry.png'];
 	game.retrybutton.y = game.height/2 + 60;
 	game.retrybutton.x = game.width/2 - 150;
 	game.retrybutton.buttonMode = "down"
-	
+
 	game.rootScene.addChild(game.retrybutton);
-	
-	// adding a back2menu button	
+
+	// adding a back2menu button
 	game.menubutton = new Sprite(300,100);
-	game.menubutton.image = game.assets['assets/back2menu.png'];
+	game.menubutton.image = game.assets['../assets/back2menu.png'];
 	game.menubutton.y = game.height/2 - 50;
 	game.menubutton.x = game.width/2 - 150;
 	game.menubutton.buttonMode = "right"
-	
+
 	game.rootScene.addChild(game.menubutton);
 
     if (playingTrumpus === true) {
