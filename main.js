65eb25f9d0fcb2a78d5778332eef6edebd0a6112
Generic <agenericgamerdude@gmail.com>
2016-10-30 11:48:41 -1000

    PR Conflict Resolution
    Manual Reset to fix PR conflicts.

--- main.js
+++ main.js
@@ -1,52 +1,3 @@
-<<<<<<< HEAD
-$(document).ready(function(){
-
-<<<<<<< HEAD
-// Displaying random banner ads below the game
-var bannerArray = [
-	{"url":"https://www.youtube.com/watch?v=POnldqfm3Rw","banner":"img/trailerad.png"},
-	{"url":"https://www.youtube.com/watch?v=POnldqfm3Rw","banner":"img/malloryad.png"},
-	{"url":"https://twitter.com/hashtag/MakeFlappyWumpusGreatAgain?src=hash","banner":"img/electionad.png"},
-	{"url":"https://soundcloud.com/cheesecast/its-time-to-softsoap","banner":"img/songad.png"},
-	{"url":"https://www.youtube.com/watch?v=xUVz4nRmxn4","banner":"img/milliondollarsad.png"},
-        {"url":"https://twitter.com/hashtag/MakeFlappyWumpusGreatAgain?src=hash","banner":"img/mfwgaad.png"}]
-
-var selectBanner = bannerArray[Math.floor(Math.random()*bannerArray.length)];
-
-$('#bottom-banner__inner').html('<a target="_blank" href="' + selectBanner.url + '"><img src="' + selectBanner.banner + '" alt="Random Banner" /></a>');
-	
-var iframeID = document.getElementById("modalIFrame"); 
-//focus the IFRAME element 
-$(iframeID).focus(); 
-//use JQuery to find the control in the IFRAME and set focus 
-$(iframeID).contents().find("#game-container").focus(); 
-=======
-	// Displaying random banner ads below the game
-	var bannerArray = [
-		{"url":"https://www.youtube.com/watch?v=POnldqfm3Rw","banner":"img/trailerad.png"},
-		{"url":"https://www.youtube.com/watch?v=POnldqfm3Rw","banner":"img/malloryad.png"},
-		{"url":"https://twitter.com/hashtag/MakeFlappyWumpusGreatAgain?src=hash","banner":"img/electionad.png"},
-		{"url":"https://soundcloud.com/cheesecast/its-time-to-softsoap","banner":"img/songad.png"},
-		{"url":"https://www.youtube.com/watch?v=xUVz4nRmxn4","banner":"img/milliondollarsad.png"},
-	    {"url":"https://twitter.com/hashtag/MakeFlappyWumpusGreatAgain?src=hash","banner":"img/mfwgaad.png"}]
-
-	var selectBanner = bannerArray[Math.floor(Math.random()*bannerArray.length)];
-
-	$('#bottom-banner__inner').html('<a target="_blank" href="' + selectBanner.url + '"><img src="' + selectBanner.banner + '" alt="Random Banner" /></a>');
-
-	function setFocusGameIframe() {
-		var iframe = $("#iframe")[0]; 
-		iframe.contentWindow.focus();
-	}
-
-	$("#game-container").click(function(){
-      setTimeout(setFocusGameIframe, 100);
-      return false;
-   });
->>>>>>> refs/remotes/origin/master
-
-});
-=======
 $(document).ready(function(){
 
 	// Displaying random banner ads below the game
@@ -73,4 +24,3 @@ $(document).ready(function(){
    });
 
 });
->>>>>>> refs/remotes/origin/master
