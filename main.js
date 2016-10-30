a2c498a9911c7ae85c4626f20bca70dc3cbe9c39
Generic <agenericgamerdude@gmail.com>
2016-10-30 11:42:17 -1000

    This should fix things
    Manual override of the merge conflicts for pull requests.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 $(document).ready(function(){
 
 <<<<<<< HEAD
@@ -45,3 +46,31 @@ $(iframeID).contents().find("#game-container").focus();
 >>>>>>> refs/remotes/origin/master
 
 });
+=======
+$(document).ready(function(){
+
+	// Displaying random banner ads below the game
+	var bannerArray = [
+		{"url":"https://www.youtube.com/watch?v=POnldqfm3Rw","banner":"img/trailerad.png"},
+		{"url":"https://www.youtube.com/watch?v=POnldqfm3Rw","banner":"img/malloryad.png"},
+		{"url":"https://twitter.com/hashtag/MakeFlappyWumpusGreatAgain?src=hash","banner":"img/electionad.png"},
+		{"url":"https://soundcloud.com/cheesecast/its-time-to-softsoap","banner":"img/songad.png"},
+		{"url":"https://www.youtube.com/watch?v=xUVz4nRmxn4","banner":"img/milliondollarsad.png"},
+	    {"url":"https://twitter.com/hashtag/MakeFlappyWumpusGreatAgain?src=hash","banner":"img/mfwgaad.png"}]
+
+	var selectBanner = bannerArray[Math.floor(Math.random()*bannerArray.length)];
+
+	$('#bottom-banner__inner').html('<a target="_blank" href="' + selectBanner.url + '"><img src="' + selectBanner.banner + '" alt="Random Banner" /></a>');
+
+	function setFocusGameIframe() {
+		var iframe = $("#iframe")[0]; 
+		iframe.contentWindow.focus();
+	}
+
+	$("#game-container").click(function(){
+      setTimeout(setFocusGameIframe, 100);
+      return false;
+   });
+
+});
+>>>>>>> refs/remotes/origin/master
